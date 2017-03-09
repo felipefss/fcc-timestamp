dba691d654b4dec596fd9c9ed56a7bdaee643a13
Felipe Santos <felipefss@gmail.com>
2017-03-09 20:54:08 +0100

    Modify port number.

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.get('/:date', (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+app.listen(80, () => {
+    console.log('Server running on port 80');
 });
