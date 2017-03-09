aa8b94bf0ef04ec6b7d896af3d077409e52e13b4
Felipe Santos <felipefss@gmail.com>
2017-03-09 18:27:12 +0100

    Implement checkDate functionality.

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const moment = require('moment');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const checkDateString = (str) => {
+    const decodedStr = decodeURI(str);
+    const date = moment(decodedStr);
+
+    if (date.isValid()) {
+        const dateObj = {
+            unix: parseInt(date.format('X')),
+            natural: date.format('MMMM DD, YYYY')
+        };
+
+        return dateObj;
+    }
+    return null;
+};
+
+app.get('/:date', (req, res) => {
+    if (req.params.date !== 'favicon.ico') {
+        const date = checkDateString(req.params.date);
+        res.send(date);
+    }
 });
 
 app.listen(3000, () => {
